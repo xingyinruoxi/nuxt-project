6ab93228aa72c5a6f0b965ea1297526864bac76e
周林 <634023699@qq.com>
2020-04-03 21:41:50 +0800

    添加异步请求

---
+++ pages/Cart/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <article>
+    <h4>商品列表</h4>
+  </article>
+</template>
+<script>
+export default {
+  async asyncData({ $axios, error }) {
+    const { ok, goods } = await $axios.$get("/api/goods");
+    if (ok) {
+      return { goods };
+    }
+    // 错误处理
+    error({ statusCode: 400, message: "数据查询失败" });
+  }
+};
+</script>
